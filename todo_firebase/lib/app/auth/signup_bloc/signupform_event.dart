3144d945586d30efdb3f7a5295939b77930f0df5
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 06:31:27 +0200

    Update signupform_event.dart

--- todo_firebase/lib/app/auth/signup_bloc/signupform_event.dart
+++ todo_firebase/lib/app/auth/signup_bloc/signupform_event.dart
@@ -3,14 +3,20 @@ part of 'signupform_bloc.dart';
 @immutable
 abstract class SignupformEvent {}
 
-class SignupWithEmailAndPassword extends SignupformEvent {
+class SignupWithEmailAndPasswordPressed extends SignupformEvent {
   final String? email;
   final String? password;
-  SignupWithEmailAndPassword({required this.email, required this.password,});
+  SignupWithEmailAndPasswordPressed({
+    required this.email,
+    required this.password,
+  });
 }
 
-class RegisterWithEmailAndPassword extends SignupformEvent {
+class RegisterWithEmailAndPasswordPressed extends SignupformEvent {
   final String? email;
   final String? password;
-  RegisterWithEmailAndPassword({required this.email, required this.password,});
+  RegisterWithEmailAndPasswordPressed({
+    required this.email,
+    required this.password,
+  });
 }
