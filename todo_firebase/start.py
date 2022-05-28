67b6fc81d06ca42afa55d05ecc9ecb78ca659a25
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 06:35:44 +0200

    Create Python-Launcher for app-development

--- todo_firebase/start.py
+++ todo_firebase/start.py
@@ -4,8 +4,8 @@ import os
 def main():
     options = { 1: 'Start Flutter',
     2: 'Self-Sign App',
-    3: 'Exit'}
-    print('Flutter ToDo App\n1) Start Emulator\n2) Read Self-Sign Key-Information\n3) Exit')
+    0: 'Exit'}
+    print('Flutter ToDo App\n1) Start Emulator\n2) Read Self-Sign Key-Information\n\n0) Exit')
     choice = int(input('Enter your choice: '))
     if choice not in options:
         print('Invalid choice')
@@ -14,10 +14,11 @@ def main():
         print('Starting Flutter Emulator ...')
         os.system('flutter doctor && flutter emulator --launch pixel')
     elif choice == 2:
-        print('Read Sign-Key')
+        print('Read Sign-Key ... ')
         os.system('keytool -list -v -alias androiddebugkey -keystore C:\debug.keystore')
-    elif choice == 3:
+    elif choice == 0:
         exit(0)
+
     
 if __name__ =='__main__':
     try: 
