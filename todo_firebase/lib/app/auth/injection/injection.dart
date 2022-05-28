4869896d3f7fdacfd90420a38358e40c9cbea63b
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 22:46:19 +0200

    Update injection.dart

--- todo_firebase/lib/app/auth/injection/injection.dart
+++ todo_firebase/lib/app/auth/injection/injection.dart
@@ -1,13 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:todo_firebase/app/auth/auth_bloc/auth_bloc.dart';
 import 'package:todo_firebase/app/auth/signup_bloc/signupform_bloc.dart';
+import 'package:todo_firebase/app/todos/observer_bloc/observer_bloc.dart';
 import 'package:todo_firebase/core/api/infrastructure/repositories/auth_repository_impl.dart';
+import 'package:todo_firebase/core/api/infrastructure/repositories/todo_repository_impl.dart';
 import 'package:todo_firebase/core/api/repositories/auth_repository.dart';
+import 'package:todo_firebase/core/api/repositories/todo_repository.dart';
 
 final sl = GetIt.I; // serivce locator
 
 Future<void> init() async {
+  //! ################ AUTH ##################
   //! state management
   sl.registerFactory(() => SignupformBloc(authRepository: sl()));
   sl.registerFactory(() => AuthBloc(authRepository: sl()));
@@ -17,4 +22,12 @@ Future<void> init() async {
   //! external
   final firebaseAuth = FirebaseAuth.instance;
   sl.registerLazySingleton(() => firebaseAuth);
+
+  //! ################# TODO ###################
+  //! repositories
+  sl.registerFactory(() => ObserverBloc(todoRepository: sl()));
+
+  sl.registerLazySingleton<TodoRepository>(() => TodoRepositoryImpl(firestore: sl()));
+  final firestore = FirebaseFirestore.instance;
+  sl.registerLazySingleton(() => firestore);
 }
