904d9fcc7ef07e5a176aa8feb1115f57b0f6b5f1
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 09:48:22 +0200

    implement routing

--- todo_firebase/lib/app/routes/router.dart
+++ todo_firebase/lib/app/routes/router.dart
@@ -2,12 +2,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:todo_firebase/home/home_page.dart';
 import 'package:todo_firebase/res/signup_page.dart';
 
 @MaterialAutoRouter (
   routes: <AutoRoute>[
     AutoRoute(page: SignUpPage, initial: true),
-
+    AutoRoute(page: HomePage, initial: false),
 
   ]
 
