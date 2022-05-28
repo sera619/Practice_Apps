25fce770321d421183f23445a94400bc23c856e6
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 06:32:28 +0200

    Update signupform_bloc.dart

--- todo_firebase/lib/app/auth/signup_bloc/signupform_bloc.dart
+++ todo_firebase/lib/app/auth/signup_bloc/signupform_bloc.dart
@@ -1,31 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:todo_firebase/core/api/repositories/auth_repository.dart';
 
 part 'signupform_event.dart';
 part 'signupform_state.dart';
 
 class SignupformBloc extends Bloc<SignupformEvent, SignupformState> {
-  SignupformBloc() : super(SignupformState(showValidationMessages: false, isSubmitting: false)) {
-    on<RegisterWithEmailAndPassword>((event, emit) {
-      if(event.email == null || event.password == null) {
+  final AuthRepository authRepository;
+  SignupformBloc({required this.authRepository}) : super(SignupformState(showValidationMessages: false, isSubmitting: false)) {
+    on<RegisterWithEmailAndPasswordPressed>((event, emit) {
+      if (event.email == null || event.password == null) {
         emit(state.copyWith(isSubbitting: false, showValidationMessages: true));
       } else {
         emit(SignupformState(showValidationMessages: false, isSubmitting: true));
       }
-
     });
-    on<SignupWithEmailAndPassword>((event, emit) {
-      if (event.email == null || event.password == null){
+    on<SignupWithEmailAndPasswordPressed>((event, emit) {
+      if (event.email == null || event.password == null) {
         emit(SignupformState(showValidationMessages: true, isSubmitting: false));
-
       } else {
         emit(SignupformState(showValidationMessages: false, isSubmitting: true));
-      
-
       }
-
     });
-
-
   }
 }
