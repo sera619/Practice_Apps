03b042d0b0d1faff9d32788dd1212694564774fa
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 09:48:31 +0200

    register AuthBloc

--- todo_firebase/lib/app/auth/injection/injection.dart
+++ todo_firebase/lib/app/auth/injection/injection.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:todo_firebase/app/auth/auth_bloc/auth_bloc.dart';
 import 'package:todo_firebase/app/auth/signup_bloc/signupform_bloc.dart';
 import 'package:todo_firebase/core/api/infrastructure/repositories/auth_repository_impl.dart';
 import 'package:todo_firebase/core/api/repositories/auth_repository.dart';
@@ -9,7 +10,7 @@ final sl = GetIt.I; // serivce locator
 Future<void> init() async {
   //! state management
   sl.registerFactory(() => SignupformBloc(authRepository: sl()));
-
+  sl.registerFactory(() => AuthBloc(authRepository: sl()));
   //! repositories
   sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(firebaseAuth: sl()));
 
