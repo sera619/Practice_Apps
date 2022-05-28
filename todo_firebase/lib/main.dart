6de87d094dd15f801e9097332ce6817aeeb7d75c
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 06:04:36 +0200

    add firebase dependencies and initialize firebase

--- todo_firebase/lib/main.dart
+++ todo_firebase/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_firebase/app/auth/injection/injection.dart' as di;
 import 'package:todo_firebase/res/signup_page.dart';
@@ -5,6 +6,7 @@ import 'package:todo_firebase/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
   runApp(const MyApp());
 }
