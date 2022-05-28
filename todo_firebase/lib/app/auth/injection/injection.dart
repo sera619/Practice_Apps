51cffa94b49cdf0c3231464776b173ffd6311b90
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 06:31:59 +0200

    injectior: register firebase Auth

--- todo_firebase/lib/app/auth/injection/injection.dart
+++ todo_firebase/lib/app/auth/injection/injection.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:todo_firebase/app/auth/signup_bloc/signupform_bloc.dart';
+import 'package:todo_firebase/core/api/infrastructure/repositories/auth_repository_impl.dart';
+import 'package:todo_firebase/core/api/repositories/auth_repository.dart';
 
 final sl = GetIt.I; // serivce locator
 
 Future<void> init() async {
   //! state management
-  sl.registerFactory(() => SignupformBloc());
+  sl.registerFactory(() => SignupformBloc(authRepository: sl()));
+
+  //! repositories
+  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(firebaseAuth: sl()));
+
+  //! external
+  final firebaseAuth = FirebaseAuth.instance;
+  sl.registerLazySingleton(() => firebaseAuth);
 }
