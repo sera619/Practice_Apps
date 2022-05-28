1593ed31198e25598a258643ca0c42950baab361
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 22:46:30 +0200

    Update todo_repository_impl.dart

--- todo_firebase/lib/core/api/infrastructure/repositories/todo_repository_impl.dart
+++ todo_firebase/lib/core/api/infrastructure/repositories/todo_repository_impl.dart
@@ -1,9 +1,17 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_firebase/core/api/errors/errors.dart';
 import 'package:todo_firebase/core/api/failures/todo_failure.dart';
 import 'package:todo_firebase/core/api/entities/todo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:todo_firebase/core/api/infrastructure/extensions/firebase_helpers.dart';
+import 'package:todo_firebase/core/api/infrastructure/models/todo_model.dart';
 import 'package:todo_firebase/core/api/repositories/todo_repository.dart';
 
 class TodoRepositoryImpl implements TodoRepository {
+  final FirebaseFirestore firestore;
+  TodoRepositoryImpl({required this.firestore});
   @override
   Future<Either<TodoFailure, Unit>> create(Todo todo) {
     // TODO: implement create
@@ -23,8 +31,21 @@ class TodoRepositoryImpl implements TodoRepository {
   }
 
   @override
-  Stream<Either<TodoFailure, List<Todo>>> watchAll() {
-    // TODO: implement watchAll
-    throw UnimplementedError();
+  Stream<Either<TodoFailure, List<Todo>>> watchAll() async* {
+    final userDoc = await firestore.userDocument();
+
+    yield* userDoc.todoCollection
+        .snapshots()
+        .map((snapshot) => Right<TodoFailure, List<Todo>>(
+            snapshot.docs.map((doc) => TodoModel.fromFirestore(doc).toDomain()).toList()))
+        .handleError((e) {
+      if (e is FirebaseException) {
+        if (e.code.contains('permission-denied') || e.code.contains("PERMISSION_DENIED")) {
+          return Left(InsufficientPermissionsError());
+          }else{
+            return Left(UnexpectedFailure());
+          }
+      }
+    });
   }
 }
