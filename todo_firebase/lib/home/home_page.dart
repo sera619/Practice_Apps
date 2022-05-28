d080fe6cc64fcc2524c3ad98265b76e60d3c2bd0
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-28 09:49:01 +0200

    Update home_page.dart

--- todo_firebase/lib/home/home_page.dart
+++ todo_firebase/lib/home/home_page.dart
@@ -1,10 +1,36 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_firebase/app/auth/auth_bloc/auth_bloc.dart';
+import 'package:todo_firebase/app/routes/router.gr.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthStateUnauthenticated) {
+              AutoRouter.of(context).push(const SignUpPageRoute());
+            }
+          },
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(icon: Icon(Icons.exit_to_app_outlined),
+          onPressed: () => {
+            BlocProvider.of<AuthBloc>(context).add(SignOutPressedEvent())
+          },),
+          title: const Text('Home Page'),
+        ),
+        body: const Placeholder(
+          
+        ),
+      ),
+    );
   }
 }
