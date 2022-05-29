163d2d6f75f47b61c46ec1bcdd4d93e09c875b6b
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-29 12:39:11 +0200

    Update home_page.dart

--- todo_firebase/lib/home/home_page.dart
+++ todo_firebase/lib/home/home_page.dart
@@ -2,33 +2,44 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_firebase/app/auth/auth_bloc/auth_bloc.dart';
+import 'package:todo_firebase/app/auth/injection/injection.dart';
 import 'package:todo_firebase/app/routes/router.gr.dart';
+import 'package:todo_firebase/app/todos/observer_bloc/observer_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocListener(
-      listeners: [
-        BlocListener<AuthBloc, AuthState>(
-          listener: (context, state) {
-            if (state is AuthStateUnauthenticated) {
-              AutoRouter.of(context).push(const SignUpPageRoute());
-            }
-          },
-        ),
+    final observerBloc = sl<ObserverBloc>()..add(ObserveAllEvent());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ObserverBloc>(
+          create: (context) => observerBloc),
       ],
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.exit_to_app_outlined),
-          onPressed: () => {
-            BlocProvider.of<AuthBloc>(context).add(SignOutPressedEvent())
-          },),
-          title: const Text('Home Page'),
-        ),
-        body: const Placeholder(
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthStateUnauthenticated) {
+                AutoRouter.of(context).push(const SignUpPageRoute());
+              }
+            },
+          ),
           
+    
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(icon: Icon(Icons.exit_to_app_outlined),
+            onPressed: () => {
+              BlocProvider.of<AuthBloc>(context).add(SignOutPressedEvent())
+            },),
+            title: const Text('Home Page'),
+          ),
+          body: const Placeholder(
+            
+          ),
         ),
       ),
     );
